c02c80170e36ba986b3c9342eb65f8510cc4271b
Jens-Peter Olsson <jens-peter.olsson@it-budet.se>
2021-12-22 16:15:16 +0100

    fix: try to avoid multiple initializations at the same time

--- src/featureToggleClient.js
+++ src/featureToggleClient.js
@@ -1,7 +1,10 @@
 const LaunchDarkly = require('launchdarkly-node-server-sdk');
-const { DynamoDBFeatureStore } = require('launchdarkly-node-server-sdk-dynamodb');
+const {
+  DynamoDBFeatureStore
+} = require('launchdarkly-node-server-sdk-dynamodb');
 
 let initialized = false;
+let initializing = false;
 let client;
 
 async function intializeLd(sdkKey) {
@@ -13,12 +16,22 @@ async function intializeLd(sdkKey) {
     sendEvents: false
   };
 
+  if (initializing) {
+    console.log('Already initializing LD, waiting 100 ms...');
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    console.log(`Initialized after waiting? ${initialized}`);
+
+    if (initialized) return;
+  }
+
   console.log('Initializing LD...');
+  initializing = true;
 
   client = LaunchDarkly.init(sdkKey, config);
   await client.waitForInitialization();
 
   console.log('Initializing LD done.');
+  initializing = false;
 
   initialized = true;
 }
